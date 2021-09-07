6448c8042086477a2b637b10c88c064962cc7e17
Manjusha <manjusha.m.nair@gmail.com>
2021-09-07 14:02:59 +0200

    Modified Temp_convertor

--- Temp_convertor.py
+++ Temp_convertor.py
@@ -1,8 +1,9 @@
 def temp_convertor(isCtoF, input_value):
-    if isCtoF == True:
+    if isCtoF == "Yes":
         output_value = (input_value * 1.8) + 32
     else:
         output_value = (input_value - 32) / 1.8
     return output_value
-
-print(temp_convertor(False, 100.4))
+user_input = float(input("Enter the temperature: "))
+isC = input("Is the value in \celsius? Type Yes or No: ")
+print("Converted value is : ", temp_convertor(isC, user_input))

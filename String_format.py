3082deaba3a56bb1b3230a03203f2766c0c6943d
Manjusha <manjusha.m.nair@gmail.com>
2021-09-08 19:48:30 +0200

    String Formatting

---
+++ String_format.py
@@ -0,0 +1,8 @@
+user_input = input("Enter your name : ")
+action = "What would you like to do today?"
+message = f"Hellooo {user_input}. {action}"
+long_emoji = '''
+o o
+ o
+'''
+print(message +  long_emoji)
